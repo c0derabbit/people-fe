f4b1817c810ecf1728ca0c3588d13391a9912834
eszter <ekov@protonmail.ch>
2021-01-10 15:13:48 +0000

    move initial fetch to server side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from 'react'
-
 import useAuth from '../hooks/use-auth'
 import useSearch from '../hooks/use-search'
 import { apiBase } from '../config'
@@ -10,25 +8,13 @@ import { Person } from '../types'
 export const Home: React.FC<{ people: Person[] }> = ({ people = [] }) => {
   const { isSignedIn, signIn } = useAuth()
   const { search } = useSearch()
-  const [clientSidePeople, setData] = useState([])
-
-  useEffect(() => {
-    // FIXME this is temporary for frontend debugging
-    const fetchData = async () => {
-      const res = await fetch(`${apiBase}/people/`)
-      const data = await res.json()
-      setData(data)
-    }
-
-    fetchData()
-  }, [])
 
   return isSignedIn
     ? (
       <>
         <SearchHeader />
         <Grid as="ul">
-          {search(clientSidePeople)?.map((person: Person) => (
+          {search(people)?.map((person: Person) => (
             <PersonCard key={person.id} {...person} />
           ))}
         </Grid>
@@ -37,13 +23,10 @@ export const Home: React.FC<{ people: Person[] }> = ({ people = [] }) => {
 }
 
 export async function getServerSideProps() {
-  /* TODO use this instead of client-side fetch once debugging is done
   const data = await fetch(`${apiBase}/people/`)
   const people = await data.json()
 
   return { props: { people } }
-   */
-  return { props: {} }
 }
 
 export default Home
