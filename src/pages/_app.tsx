5b53285f79fb2232ffd85a07248f6aaa3f7e4113
eszter <ekov@protonmail.ch>
2021-01-10 11:29:12 +0000

    fix serverside stylesheets

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
 
 import { AuthProvider } from '../hooks/use-auth'
 import { SortSearchProvider } from '../hooks/use-sort-search'
@@ -7,7 +8,22 @@ import { Header } from '../components'
 import { Container } from '../styled'
 
 import 'modern-normalize/modern-normalize.css';
-import '../styled/settings.css'
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    color: mediumslateblue;
+    --primary: mediumslateblue;
+    --primary-darker: #6e5dd6;
+    --primary-lighter: #bbb1f6;
+    --light-accent: lavender;
+    --lighter-accent: #f7f7fd;
+
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+  }
+`
 
 function App({ Component, pageProps }) {
   return (
@@ -17,6 +33,7 @@ function App({ Component, pageProps }) {
           <title>Peoplegraph</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
+        <GlobalStyle />
         <Header />
         <Container as="main">
           <Component {...pageProps} />
