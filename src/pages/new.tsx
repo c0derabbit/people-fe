4122c118a8b96179643b7f8a669e0ff3e7319848
eszter <ekov@protonmail.ch>
2021-01-11 21:16:15 +0000

    Update new.tsx

--- src/pages/new.tsx
+++ src/pages/new.tsx
@@ -1,10 +1,5 @@
 import Form from '../components/form'
 
 export default function New() {
-  return (
-    <>
-      <h2>Create a new contact</h2>
-      <Form />
-    </>
-  )
+  return <Form />
 }
